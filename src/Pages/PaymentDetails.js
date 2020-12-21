cf3bb38796c7163215eaf87ef36cd4dd8a237f1f
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-12-21 15:15:11 +0530

    new pages added

---
+++ src/Pages/PaymentDetails.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+import { Navbar } from '../Components/Navbar';
+
+export const PaymentDetails = () => {
+    return (
+        <Fragment>
+        </Fragment>
+  );
+};
