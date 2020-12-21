784f326d6c8e8ff1b50ded71009a5cd8315b37ee
kartikeya72001 <kartikeya72001@gmail.com>
2020-12-21 22:20:47 +0530

    OrderSummary-Section_1

--- src/Pages/OrderSummary.js
+++ src/Pages/OrderSummary.js
@@ -29,22 +29,90 @@ export const OrderSummary = () => {
                         </div>
                     </div>
                   </div>
-                  <img src='image/rating/r4.svg' alt='' />
-                  <p>
-                    Marchant Since :
-                    <span className='text-right'> OCT 2019</span>
-                  </p>
-                  <p>
-                    Location :<span className='text-right'> Gurgaon</span>
-                  </p>
-                  <p>
-                    Total Product in stock :{' '}
-                    <span className='text-right'> 294</span>
-                  </p>
-                  <p>
-                    Total product sold till date :{' '}
-                    <span className='text-right'> 29324</span>
-                  </p>
+                  <div className="container" style={{marginTop:"5%"}}>
+                    <div className="row">
+                      <div className="col-3">
+                      <h5 style={{
+                        fontSize:"15px",
+                        color:"lightgray",
+                      }}>Quantity</h5>
+                      </div>
+                      <div className="col-4">
+                      <select id="cars" name="cars">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                      </select>
+                      </div>
+                      <div className="col-5"></div>
+                    </div>
+                  </div>
+                  <div className="container" style={{marginTop:"5%"}}>
+                    <div className="row">
+                      <div className="col-3">
+                      <h5 style={{
+                        fontSize:"15px",
+                        color:"gray",
+                      }}>Condition</h5>
+                      </div>
+                      <div className="col-4">
+                      <button type="button" style={{
+                        fontSize:"12px",
+                        backgroundColor: "darkgreen",
+                        color: "white",
+                        borderRadius: "2px",
+                        padding: "5px",
+                        width:"96px"
+                      }} class="btn">Renewed</button>
+                      </div>
+                      <div className="col-5"></div>
+                    </div>
+                  </div>
+                  <div className="container" style={{marginTop:"5%"}}>
+                    <div className="row">
+                      <div className="col-2">
+                        <h5><b><span>&#8377;</span>5899</b></h5>
+                      </div>
+                      <div className="col-2">
+                        <h5 style={{
+                          marginTop:"7px",
+                          marginLeft: "5px",
+                          color:"gray",
+                          fontSize:"12px"
+                        }}><strike><span>&#8377;</span>8999</strike></h5>
+                      </div>
+                      <div className="col-3">
+                        <h6 style={{color:"red", fontSize:"12px", marginTop:"7px"}}>50% OFF</h6>
+                      </div>
+                    </div>
+                  </div>
+                  <h5 style={{
+                          marginTop:"7px",
+                          marginLeft: "5px",
+                          color:"gray",
+                          fontSize:"12px"
+                        }}>Free Delivery in 0-2 Days</h5>
+                  <h5 style={{
+                          marginTop:"7px",
+                          marginLeft: "5px",
+                          color:"gray",
+                          fontSize:"10px"
+                        }}>Buying From</h5>
+                  <i class='bx bxs-cart'></i><span style={{
+                    fontSize:"12px",
+                    marginLeft:"5px",
+                  }}>Cart-N-You Pvt Ltd.</span>
+                  <br></br>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-12">
+                        <i class='bx bxs-x-circle' style={{color:"#c2172e", fontSize:"18px"}}></i> <span style={{fontSize:"14px"}}>Cash On Delivery</span><br></br>
+                        <i class='bx bxs-check-circle' style={{color:"green", fontSize:"18px"}}></i> <span style={{fontSize:"14px"}}>10 day Replacement Policy</span><br></br>
+                        <i class='bx bxs-check-circle' style={{color:"green", fontSize:"18px"}}></i> <span style={{fontSize:"14px"}}>Free Shopping</span><br></br>
+                      </div>
+                    </div>
+                  </div>
                 </div>
             </section>
           </div>
