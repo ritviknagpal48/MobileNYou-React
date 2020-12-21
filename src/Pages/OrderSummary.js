c046595857ee4319928fd499d57a5d02f0c169c6
kartikeya72001 <kartikeya72001@gmail.com>
2020-12-21 21:45:37 +0530

    OrderSummary

--- src/Pages/OrderSummary.js
+++ src/Pages/OrderSummary.js
@@ -9,128 +9,43 @@ export const OrderSummary = () => {
         <div class='universalDiv universal-Div'>
           <div class='container x-container'>
             <Navbar></Navbar>
-            <section className='section1'>
-              <div class='where-cont'>
-                <div class='where'>
-                  <p class=' c c1'>1</p>
-                  <p>Add Address</p>
-                </div>
-                <div class=' hr hr1'></div>
-                <div class='where'>
-                  <p class='c c2'>2</p>
-                  <p>Order Summary</p>
-                </div>
-                <div class=' hr hr2'></div>
-                <div class='where'>
-                  <p class='c c3'>3</p>
-                  <p>Payment</p>
-                </div>
-              </div>
-              <div>
-                <div className='address-section'>
-                  <div className='user-name'>
-                    <p>Neeraj Sharma</p>
-                    <p className='type'>office</p>
+            <section className="bodypanelmian aboutseller text-left">
+            <div className='sellerlocation'>
+                  <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                        <img src="/image/RealmeX50Pro.jpg"></img>
+                        </div>
+                        <div className="col-7" style={{
+                          marginTop: "3%",
+                        }}>
+                          <h3>Realme X50 Pro 5G(Rust Red, 256GB) (12GB RAM)</h3>
+                          <h6 style={{
+                            color: "gray",
+                            fontSize: "10px",
+                          }}>4GB 128GB</h6>
+                        </div>
+                        <div className="col-2">
+                        </div>
+                    </div>
                   </div>
-                  <p className='address'>
-                    Rzg-261,Sitapuri,Street No. 13,New Delhi,Delhi 110045
+                  <img src='image/rating/r4.svg' alt='' />
+                  <p>
+                    Marchant Since :
+                    <span className='text-right'> OCT 2019</span>
                   </p>
-                  <p className='phone-no'>+91-97167281-70</p>
-                  <button>Change or Add Address</button>
-                </div>
-                <div className='product-container'>
-                  <div className='product-img-name'>
-                    <img
-                      src='image/home/flash/img1.png'
-                      className='img-fluid'
-                    />
-                    <div className='name-spec'>
-                      <p>Realme Mobile Ram 4GB, 64GB, 660 Processer...</p>
-                      <p>Red 4gb 128gb</p>
-                    </div>
-                  </div>
-                  <div className='quantity'>
-                    <label>Qty</label>
-                    <select>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                    </select>
-                  </div>
-                  <div className='condition'>
-                    <p>Condition:</p>
-                    <span>Renewed</span>
-                  </div>
-                  <div className='price1'>
-                    <p>₹ 5999</p>
-                    <s>8999</s>
-                    <p>50% OFF</p>
-                  </div>
-                  <div className='del-time'>
-                    <p>Free delivery in 0-2 days</p>
-                  </div>
-                  <div className='buying-details'>
-                    {/* <span>Buying From</span>
-                            <div class="option">
-                                <p>&#1F6D6</p>
-                                <p>Cart-N-you Pvt.Ltd</p>
-                            </div> */}
-                    <div className='option'>
-                      <p id='not'>✖</p>
-                      <p>Cash on Delivery</p>
-                    </div>
-                    <div className='option'>
-                      <p>✔</p>
-                      <p>10 Days Replacements Policy</p>
-                    </div>
-                    <div className='option'>
-                      <p>✔</p>
-                      <p>Free Shipping</p>
-                    </div>
-                  </div>
-                </div>
-                <div className='gst-details'>
-                  <div className='add-GST'>
-                    <div>
-                      <input type='checkbox' />
-                      <label>Add GST details</label>
-                    </div>
-                    <a className='edit-gst'>Edit</a>
-                  </div>
-                  <p>di3n543i53454n63</p>
-                  <p>Cartnyou Retail Private Limited</p>
-                </div>
-                <div className='price-details'>
-                  <p className='price-detail-tag'>Price Details</p>
-                  <div className='prices'>
-                    <div className='pr amount'>
-                      <p>Price(9 items):</p>
-                      <p>₹ 5000</p>
-                    </div>
-                    <div className='pr Shipping-price'>
-                      <p>Shipping Cost</p>
-                      <p className='free-ship'>Free Shipping</p>
-                    </div>
-                    <div className='pr discount'>
-                      <p>Discount</p>
-                      <p>-₹ 500</p>
-                    </div>
-                    <div className='pr MNU Money'>
-                      <p>MNU Money</p>
-                      <p>-₹ 500</p>
-                    </div>
-                  </div>
-                  <div className='pr total-price'>
-                    <p>Total</p>
-                    <p>₹ 4000</p>
-                  </div>
-                  <p className='money'>
-                    you will save <span className='money-save'>₹ 500 </span>on
-                    this order
+                  <p>
+                    Location :<span className='text-right'> Gurgaon</span>
+                  </p>
+                  <p>
+                    Total Product in stock :{' '}
+                    <span className='text-right'> 294</span>
+                  </p>
+                  <p>
+                    Total product sold till date :{' '}
+                    <span className='text-right'> 29324</span>
                   </p>
                 </div>
-              </div>
             </section>
           </div>
         </div>
