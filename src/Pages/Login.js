442ecdbbab6888bfcf19dc5724877212704bea44
kartikeya72001 <kartikeya72001@gmail.com>
2020-12-22 13:37:17 +0530

    Head

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 
 export const Login = () => {
-    return (
-      <Fragment>
-            <div class="container" style={{dispaly:"flex",justifyContent:"center",alignItems:"Center",flexDirection:"column"}}>
-                <img src="image/header/logo.gif" classname="img-fluid"></img>
-            </div>    
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <i class='bx bx-arrow-back' style={{fontSize:"27px", margin:"5%"}}></i>
+      <div class="container" style={{
+        textAlign: "center",
+        dispaly: "flex",
+        justifyContent: "center",
+        alignItems: "Center",
+        flexDirection: "column",
+      }}>
+        <img src="image/header/logo.gif" alt="Logo" classname="img-fluid"></img>
+      </div>
+    </Fragment>
+  );
 };
