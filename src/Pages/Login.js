2a194573eb532c47521c6ff176a35b858302dba5
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-12-22 13:31:02 +0530

    logo added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,7 +3,9 @@ import React, { Fragment } from 'react';
 export const Login = () => {
     return (
       <Fragment>
-        
+            <div class="container" style={{dispaly:"flex",justifyContent:"center",alignItems:"Center",flexDirection:"column"}}>
+                <img src="image/header/logo.gif" classname="img-fluid"></img>
+            </div>    
         </Fragment>
     );
 };
