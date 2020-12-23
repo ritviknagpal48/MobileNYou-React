5a3ceb9e3524c987cb218391eeba347b91e37140
Shreyans Jain <jainshreyans121@gmail.com>
2020-12-23 21:57:34 +0530

    Payment Fail

--- src/Pages/Transactionerror.js
+++ src/Pages/Transactionerror.js
@@ -6,11 +6,20 @@ export const Transactionerror = () => {
         <Fragment>
             <div class="badydiv">
                 <div class="universalDiv">
-                    <div class="container" style={{ backgroundColor: "white" }}></div>
+                    <div class="container" style={{ backgroundColor: "#f5f5f5", height: '100vh' }}>
                     <Navbar />
+                    <section class="bodypanelmian">
+                        <img src="/icons/fail.png" style={{width:"50%",height:"auto", marginTop:'50%'}}></img>
+                        <div>
+                            <p style={{color:'#c2172e', fontSize:'18px', marginTop:'5%'}}><b>Transaction Failed</b></p>
+                            <p style={{color:'grey', fontSize:'12px'}}>Sorry, Your Transaction Has Been Failed. Please Try Again</p>
+                        </div>
+                        
+                        <button className="btn" style={{ marginTop: '50%', padding: "0% 15%", border: "#c2172e solid 2px", backgroundColor: "white", color: "#c2172e",height:"3rem" }}><b><span style={{float:"left"}}>Go to Cart</span> <i style={{fontSize:"24px",float:"left"}} class='bx bx-right-arrow-alt'></i></b></button>
+                    </section>
+                    </div>
                 </div>
             </div>
-            <section class="bodypanelmian"></section>
         </Fragment>
     );
 };
