0be20bc0bbd11052997afa8f64fac2759719971a
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-12-23 22:21:44 +0530

    order placed page added

--- src/Pages/Orderplaced.js
+++ src/Pages/Orderplaced.js
@@ -8,16 +8,16 @@ export const Orderplaced = () => {
                 <div class="universalDiv graybg" style={{ backgroundColor: "#F5F5F5" }}>
                     <Navbar />
                     <section class="bodypanelmian">
-                        <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",marginTop:"1rem",backgroundColor:"white",boxShadow: "0px 0px 4px 1px gainsboro"}}>
-                            <img src="/icons/tick.png" style={{width:"45%",height:"auto"}}></img>
+                        <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",marginTop:"1rem",backgroundColor:"white",boxShadow: "0px 0px 4px 1px gainsboro",backgroundImage:"url(/icons/party.png)"}}>
+                            <img src="/icons/tick.png" style={{width:"45%",height:"auto",marginTop:"1rem"}}></img>
                             <p style={{fontSize:"23px",fontWeight:"bolder",marginTop:"1rem"}}>Your Shipping Order</p>
                             <p style={{fontSize:"23px",fontWeight:"bolder"}}>Has Been Placed</p>
-                            <p style={{fontSize:"12px",color:"grey"}}>Thank You For Order. We Are Processing Your Order</p>
+                            <p style={{fontSize:"12px",color:"grey",margin:"0.3rem 0 1rem 0"}}>Thank You For Order. We Are Processing Your Order</p>
                         </div>
                         <div class="sellerlocation" style={{backgroundColor:"white",height:"10rem",marginTop:"1rem",boxShadow: "0px 0px 4px 1px gainsboro"}}>
                             <p style={{fontSize:"14px",float:"left",margin:"0.2rem 0.7rem"}}>Delivery by Fri, Dec 4th 20</p>
                             <br></br>
-                            <hr style={{ margin: '1% 0' }} />
+                            <hr style={{ margin: '1% 0.7rem' }} />
                             <p style={{fontSize:"16px",fontWeight:"bold",float:"left",marginLeft:"0.7rem"}}>Neeraj Sharma</p>
                             <br></br>
                             <p style={{fontSize:"13px",float:"left",marginLeft:"0.7rem",color:"grey"}}>Rzg-261,Sitapuri-2, Street No. 13,</p>
@@ -26,7 +26,7 @@ export const Orderplaced = () => {
                             <br></br>
                             <p style={{fontSize:"13px",float:"left",marginLeft:"0.7rem",color:"grey"}}>110045</p>
                             <br></br>
-                            <p style={{color:"#c2172e",fontSize:"20px",fontWeight:"bold"}}>View Details</p>
+                            <p style={{color:"#c2172e",fontSize:"18px",fontWeight:"bold"}}>View Details</p>
                         </div>
                         <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",marginTop:"55%"}}>
                             <button className="btn" style={{ padding: "4% 3%", border: "#c2172e solid 2px", borderRadius:"5px", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", width: "58%", backgroundColor: "white", color: "#c2172e",height:"2rem" }}><b><span style={{float:"left"}}>Continue Shopping</span> <i style={{fontSize:"24px",float:"left"}} class='bx bx-right-arrow-alt'></i></b></button>
