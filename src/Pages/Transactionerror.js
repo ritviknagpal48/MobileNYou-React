793a493dafbb02d9b36cc547c02cab590083a700
kartikeya72001 <kartikeya72001@gmail.com>
2020-12-23 22:39:26 +0530

    OrderPlaced

--- src/Pages/Transactionerror.js
+++ src/Pages/Transactionerror.js
@@ -9,7 +9,7 @@ export const Transactionerror = () => {
                     <div class="container graybg" style={{ backgroundColor: "#f5f5f5"}}>
                     <Navbar />
                     <section class="bodypanelmian">
-                        <img src="/icons/fail.png" style={{width:"50%",height:"auto", marginTop:'50%'}}></img>
+                        <img src="/icons/Fail.png" style={{width:"50%",height:"auto", marginTop:'50%'}}></img>
                         <div>
                             <p style={{color:'#c2172e', fontSize:'18px', marginTop:'5%'}}><b>Transaction Failed</b></p>
                             <p style={{color:'grey', fontSize:'12px'}}>Sorry, Your Transaction Has Been Failed. Please Try Again</p>
