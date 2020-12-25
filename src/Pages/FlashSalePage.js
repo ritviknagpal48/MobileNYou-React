2db1b3cdae72e6f8bd02f0d080efbe180a0fe5af
kartikeya72001 <kartikeya72001@gmail.com>
2020-12-25 14:40:41 +0530

    SOrt-Filter

--- src/Pages/FlashSalePage.js
+++ src/Pages/FlashSalePage.js
@@ -11,37 +11,39 @@ export const FlashSalePage = () => {
       <div class='badydiv'>
         <div class='universalDiv'>
           <Navbar></Navbar>
-          <SortFilter given="true"></SortFilter>
-          <div className='flashsale'>
-          <div className='row no-gutters'>
-          <div className='col'>
-            <div className='heing'>
-              <h2>Flash Sale</h2>
-            </div>
-            <div className='d-flex colan justify-content-center'>
-              <div className='timer'>
-                <p className='tm'>02</p>
-                <p className='dd'>DD</p>
-              </div>
-              <span className='coln'>:</span>
-              <div className='timer'>
-                <p className='tm'>12</p>
-                <p className='dd'>HH</p>
-              </div>
-              <span className='coln'>:</span>
-              <div className='timer'>
-                <p className='tm'>24</p>
-                <p className='dd'>MM</p>
-              </div>
-              <span className='coln'>:</span>
-              <div className='timer'>
-                <p className='tm'>38</p>
-                <p className='dd'>SS</p>
+          <div style={{position:"sticky", top:"0", zIndex:"100"}}>
+            <SortFilter given="true"></SortFilter>
+            <div className='flashsale' style={{backgroundColor:"white", borderBottom:"1px gray solid"}}>
+              <div className='row no-gutters'>
+                <div className='col'>
+                  <div className='heing'>
+                    <h2>Flash Sale</h2>
+                  </div>
+                  <div className='d-flex colan justify-content-center'>
+                    <div className='timer'>
+                      <p className='tm'>02</p>
+                      <p className='dd'>DD</p>
+                    </div>
+                    <span className='coln'>:</span>
+                    <div className='timer'>
+                      <p className='tm'>12</p>
+                      <p className='dd'>HH</p>
+                    </div>
+                    <span className='coln'>:</span>
+                    <div className='timer'>
+                      <p className='tm'>24</p>
+                      <p className='dd'>MM</p>
+                    </div>
+                    <span className='coln'>:</span>
+                    <div className='timer'>
+                      <p className='tm'>38</p>
+                      <p className='dd'>SS</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        </div>
 
           <section className='bodypanelmian' style={{ marginBottom: '50px' }}>
             <div className='container'>
