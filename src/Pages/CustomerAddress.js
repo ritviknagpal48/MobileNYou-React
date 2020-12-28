142ea22754c4cf0612b6dc9ae3d5ab4ce52932f9
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-12-28 22:51:43 +0530

    navbar in customeraddress

--- src/Pages/CustomerAddress.js
+++ src/Pages/CustomerAddress.js
@@ -8,6 +8,13 @@ export const CustomerAddress = () => {
         <div class='badydiv'>
             <div class='universalDiv'>
             <section className="bodypanelmian aboutseller text-left" style={{ marginBottom: '20%', paddingBottom: '10px' }}>
+                <div style={{width:"100%",height:"3rem",backgroundColor:"#c2172e",color:"white"}}>
+                    <i class='bx bx-arrow-back' style={{fontSize: "1.3rem",
+                                                        margin: "0.7rem 0.3rem",
+                                                        position: "relative",
+                                                        top: "0.1rem"}}></i>                    
+                    <span style={{fontSize:"1.3rem"}}>Addresses</span>
+                </div>
                 <SingleCustomerAddress></SingleCustomerAddress>
                 <SingleCustomerAddress></SingleCustomerAddress>
                 <SingleCustomerAddress></SingleCustomerAddress>
