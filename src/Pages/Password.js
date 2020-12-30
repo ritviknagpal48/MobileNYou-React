18b81adb3c5998c9850ca0650b39309e2f4dcbad
Shreyans Jain <jainshreyans121@gmail.com>
2020-12-30 22:25:49 +0530

    password-header

--- src/Pages/Password.js
+++ src/Pages/Password.js
@@ -6,10 +6,24 @@ export const Password = () => {
     return (
         <Fragment>
             <div class="badydiv">
-                <div class="universalDiv" style={{height: '100vh'}}>
+                <div class="universalDiv" style={{height: '100vh', backgroundColor: '#f5f5f5'}}>
                     <RedNavbar navbar="Change Password"></RedNavbar>
                     <section className="bodypanelmian aboutseller text-left">
-                        
+                      <div style={{height:"5rem"}}>
+                        <div class="row" style={{height:"100%"}}>
+                            <div class="col-4" style={{display:"flex",padding:"none"}}>
+                              <div class="customer-account-image">N</div>                              
+                            </div>
+                            <div class="col-5 customer-account-details" style={{marginLeft:"-3.5rem",marginRight:"3.5rem"}}>
+                                <p style={{fontSize:"18px",marginTop:"0.3rem"}}>NEERAJ</p>
+                                <p style={{fontSize:"14px"}}>nesh.28feb@gmail.com</p>
+                                <p style={{fontSize:"14px"}}>+91-9123-4101-01</p>
+                            </div>
+                            <div class="col-3"> 
+                            </div>
+                        </div>
+                      </div>
+                      
                     </section>
                 </div>
             </div>
