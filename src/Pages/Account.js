9b9b921b274a61fab6edb53b4c4e4a0eb55e39e7
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-12-30 17:00:20 +0530

    account profile in account page

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -8,6 +8,23 @@ export const Account = () => {
             <div class="badydiv">
                 <div class="universalDiv" >
                     <RedNavbar></RedNavbar>
+                    <section className="bodypanelmian aboutseller text-left" >
+                        <div style={{height:"5rem"}}>
+                            <div class="row" style={{height:"100%"}}>
+                                <div class="col-4" style={{display:"flex",padding:"none"}}>
+                                    <div class="customer-account-image">N</div>                            
+                                </div>
+                                <div class="col-5 customer-account-details" style={{marginLeft:"-3.5rem",marginRight:"3.5rem"}}>
+                                    <p style={{fontSize:"18px",marginTop:"0.3rem"}}>NEERAJ</p>
+                                    <p style={{fontSize:"14px"}}>nesh.28feb@gmail.com</p>
+                                    <p style={{fontSize:"14px"}}>+91-9123-4101-01</p>
+                                </div>
+                                <div class="col-3">
+                                    <button type="button" class="btn btn-outline-danger" style={{margin:"1rem 0",borderColor:"#c2172e",color:"#c2172e",padding:"0.4rem 1rem"}}>Edit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </Fragment>
