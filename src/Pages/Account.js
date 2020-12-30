d1b742115e33f219a156a10ea16edda9a322923e
Shreyans Jain <jainshreyans121@gmail.com>
2020-12-30 17:09:30 +0530

    accounts page btns

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -24,6 +24,94 @@ export const Account = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <hr />
+
+                        <div class="container">
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bx-wallet' style={{color:'#c2172e', marginTop: '0.2rem'}} ></i>
+                                    </div>
+                                    <div class="col-8" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        MNU Wallet
+                                    </div>
+                                    <div class="col-2" style={{paddingLeft: '0'}}>
+                                        <span style={{color: 'green'}}> &#8377;500</span>
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bxl-dropbox' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Orders
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bx-heart' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Wishlist
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bx-support' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Support Center
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bxs-edit-location' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Saved Address
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bxs-coupon' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Coupons
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bx-money' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Refer and Earn
+                                    </div>
+                                </div>
+                            </button>
+                            <button class="transparent-btn">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <i class='bx bx-log-out' style={{color:'#c2172e', marginTop: '0.2rem', fontSize: '20px'}} ></i>
+                                    </div>
+                                    <div class="col-10" style={{textAlign: 'left', paddingLeft: '0'}}>
+                                        Logout
+                                    </div>
+                                </div>
+                            </button>
+                        </div>
                     </section>
                 </div>
             </div>
