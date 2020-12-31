41b4d0f6d7ed00b65d216666144b930890cd3320
Shreyans Jain <jainshreyans121@gmail.com>
2020-12-31 15:21:18 +0530

    Orders shipping and price details

--- src/Pages/OrderDetails.js
+++ src/Pages/OrderDetails.js
@@ -22,11 +22,65 @@ export const OrderDetails = () => {
                                 </div>
                             </div>
                         </div>
+                    </section>
+                    <section class="bodypanelmian aboutseller text-left">
                         <div class="sellerlocation" style={{backgroundColor:"white",height:"2.8rem"}}>
                             <p><span style={{float:"left"}}>Cancel Order</span><span style={{float:"right"}}><i class='bx bxs-chevron-right'></i></span></p>
                         </div>
                         <div class="sellerlocation" style={{backgroundColor:"white",height:"2.8rem"}}>
                             <p><span style={{float:"left"}}>Support Center</span><span style={{float:"right"}}><i class='bx bxs-chevron-right'></i></span></p>
+                        </div>
+                        <div className='sellerlocation' style={{backgroundColor: '#fff'}}>
+                            <p style={{ color: 'grey'}}>
+                            Shipping Address:
+                            </p>
+                            <hr style={{ margin: '1% 0' }} />
+                            <h3 style={{marginTop: '0.4rem'}}>
+                            <b>Neeraj Sharma <span className='text-right' style={{ backgroundColor: 'lightgray', padding: '2px 10px', fontSize: '10px' }}>Office</span></b>
+                            </h3>
+                            <p style={{margin: '0'}}>
+                                36476  PfannerStill, Lokhandwala
+                            </p>
+                            <p style={{margin: '-1% 0'}}>
+                                Charminar 68761
+                            </p>
+                            <p style={{margin: '0'}}>
+                                Delhi - 110045
+                            </p>
+                            <p>
+                                Phone No. : +91-9123-4101-01
+                            </p>
+                        </div>
+                        <div className='sellerlocation' style={{backgroundColor: '#fff'}}>
+                            <p style={{ color: 'grey' }}>
+                            Price Details
+                            </p>
+                            <hr style={{ margin: '1% 0' }} />
+                            <p>
+                                MRP
+                                <span className='text-right' style={{ float: 'right' }}>&#8377; 5000</span>
+                            </p>
+
+                            <p>
+                                Item Discount
+                                <span className='text-right' style={{ float: 'right' }}> (&minus;) &#8377; 500</span>
+                            </p>
+
+                            <p>
+                                Card Payment
+                                <span className='text-right' style={{ float: 'right' }}> &#8377; 4500</span>
+                            </p>
+                            <p>
+                                Shipping Cost
+                                <span className='text-right' style={{ float: 'right'}}> 
+                                    Free Shipping 
+                                </span>
+                            </p>
+                            <hr style={{ margin: '2% 0' }} />
+                            <p>
+                            Total
+                                <span className='text-right' style={{ float: 'right' }}> &#8377; 4500</span>
+                            </p>
                         </div>
                     </section>
                 </div>
