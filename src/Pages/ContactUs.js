95d0542e876743666d1e0826bed22988755a4d55
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2021-01-02 16:17:54 +0530

    contact us

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -13,37 +13,30 @@ export const ContactUs = () => {
         <Fragment>
             <div class="badydiv">
                 <div class="universalDiv" style={{height: '100vh',backgroundColor:"#F5F5F5"}}>
-                    <RedNavbar navbar="Support Center"></RedNavbar>
+                    <RedNavbar navbar="Contact Us"></RedNavbar>
                     <section className="bodypanelmian aboutseller text-left">
-                     <div class="sellerlocation" style={{backgroundColor:"white"}}>
-                            <div class="row">
-                                <div className="col-3">
-                                    <img src='/image/RealmeX50Pro.jpg' className='img-fluid' alt='img1' />
-                                </div>
-                                <div className="col-9" style={{textAlign: 'left'}}>
-                                    <p style={{lineHeight:"1rem",marginBottom:"0.4rem"}}>Realme X50 Pro 5G (Rust Red, 256GB)<br></br> (12GB RAM)</p>
-                                    <p><span style={{float:"left"}}><i className='fas fa-rupee-sign' /> 599</span></p>
-                                </div>
-                            </div>
-                        </div>
                     <div class="sellerlocation" style={{backgroundColor:"white"}}>
-                             <p style={{ color: 'grey',fontSize:"13px"}}>
-                                PLEASE SELECT AN ISSUE
+                             <p style={{fontSize:"14px"}}>
+                                MY RETURN WAS PICKED UP BUT I HAVEN'T RECIEVED MY REFUND YET 
                             </p>
                             <hr style={{ margin: '1% 0' }} />
-                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>I want to know the status of my order</p>
-                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>My order is delayed</p>
-                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>I want my order to be delivered at a specific date/time</p>
-                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>I paid for my order but my order status shows payment not recieved</p>
-                            <p onClick={Expand} style={{color:"#c2172e",textAlign:"center",fontSize:"16px",cursor:"pointer"}}>View All</p>
-                            <div id="expand-issue-list" style={{display:"none"}}>
-                                <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>I paid for my order but my order status shows payment not recieved</p>
-                                <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>I paid for my order but my order status shows payment not recieved</p>
-                                <p style={{margin:"1rem 0 0 0",fontSize:"13px",cursor:"pointer"}}>Other</p>
-                            </div>
+                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>Refund will be initiated after we recieve the item and it passes the quantity check.</p>
+                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>Refund time depends on the mode of refund</p>
+                            <p style={{margin:"1rem 0",fontSize:"13px",cursor:"pointer"}}>Refund time depends on the mode of refund</p>
+                            <p style={{margin:"1rem 0 0 0",fontSize:"13px",cursor:"pointer"}}>Bank Account</p>
+                            <p style={{margin:"0 0 1rem 0",fontSize:"13px",cursor:"pointer"}}>Time the item takes to reach us +1 to 3 business days.</p>
+                            <p style={{margin:"1rem 0 0 0",fontSize:"13px",cursor:"pointer"}}>Online Refund</p>
+                            <p style={{margin:"0 0 1rem 0",fontSize:"13px",cursor:"pointer"}}>Time the item takes to reach us +7 to 10 business days.</p>
+                            <p style={{margin:"1rem 0 0 0",fontSize:"13px",cursor:"pointer"}}>PhonePe Wallet</p>
+                            <p style={{margin:"0",fontSize:"13px",cursor:"pointer"}}>Time the item takes to reach us +1 business day.</p>
                         </div>
-                        <div className="submit-button" style={{position:"relative", display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column", top: '0rem !important'}}>
-                            <button type="button" class="btn btn-danger" style={{backgroundColor:" #c2172e",width:"92%", marginBottom: '2rem'}}>Save Changes</button>
+                        <div class="sellerlocation" style={{backgroundColor:"white"}}>
+                             <p style={{fontSize:"13px",color:"grey"}}>
+                                STILL NEED HELP?
+                            </p>
+                            <hr style={{ margin: '1% 0' }} />
+                            <p style={{margin:"0",fontSize:"13px",cursor:"pointer"}}>Have queries? please get in touch and we will be happy to help you</p>
+                            <p style={{margin:"0",fontSize:"15px",cursor:"pointer",color:"#c2172e"}}>Contact Us <i class='bx bxs-chevron-right' style={{position:'relative',top:"0.07rem"}}></i></p>
                         </div>
                     </section>
                 </div>
