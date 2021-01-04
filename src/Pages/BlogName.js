5d3886c43ea4cee995058c73e925d81dfa778ea5
kartikeya72001 <kartikeya72001@gmail.com>
2021-01-04 18:17:41 +0530

    Image

--- src/Pages/BlogName.js
+++ src/Pages/BlogName.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Fragment } from 'react';
 import { Navbar } from '../Components/Navbar';
 
@@ -6,8 +7,19 @@ export const BlogName = () => {
     return (
         <Fragment>
             <div class="badydiv">
-                <div class="universalDiv" style={{backgroundColor:"#F5F5F5"}}>
+                <div class="universalDiv" style={{ backgroundColor: "#f5f5f5" }}>
                     <Navbar></Navbar>
+                    <section className="bodypanelmian aboutseller text-left">
+                        <div className="sellerlocation" style={{backgroundColor:"white",}}>
+                            <img src="/image/m-product/s10plus.jpg" style={{ width: "100%" }}></img>
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-8"><h6><b>Business of Mobiles</b></h6></div>
+                                    <div className="col-4" style={{color:"gainsboro"}}><p>5 min read</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </Fragment>
