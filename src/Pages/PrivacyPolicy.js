a75f96b1e223f0724b00904806a586d37d576de6
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2021-01-04 13:17:01 +0530

    Privacy Policy

--- src/Pages/PrivacyPolicy.js
+++ src/Pages/PrivacyPolicy.js
@@ -7,8 +7,50 @@ export const PrivacyPolicy = () => {
     return (
         <Fragment>
             <div class="badydiv">
-                <div class="universalDiv" style={{backgroundColor:"#F5F5F5"}}>
+                <div class="universalDiv" style={{backgroundColor:"white"}}>
                     <Navbar></Navbar>
+                    <section className="bodypanelmian aboutseller text-left" style={{marginBottom:"1rem"}}>
+                        <div className="strike-color" style={{ textAlign: "center", marginTop: "10px", marginBottom: "24px" }}>
+                            <h6 className="strike-through-heading"><span style={{margin:'0 0.2rem'}}>Privacy Policy</span></h6>
+                        </div>
+                        <p style={{margin:"0 1rem",fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                        <div style={{margin:"2rem 1rem"}}>
+                            <p style={{textAlign:"left"}}>i. ACCEPTANCE</p>
+                            <ol style={{margin:"0 0.8rem"}}>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                            </ol>
+                        </div>
+                        <div style={{margin:"2rem 1rem"}}>
+                            <p>ii. DESCRIPTION OF SERVICE AND CONTENT POLICY</p>
+                            <p style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <ul style={{margin:"0 0.8rem"}}>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                            </ul>
+                        </div>
+                        <div style={{margin:"2rem 1rem"}}>
+                            <p>iii. CONDUCT</p>
+                            <p style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div style={{margin:"2rem 1rem"}}>
+                            <p>iv. PAID SERVIES</p>
+                            <p style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <ul style={{margin:"0 0.8rem"}}>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                                <li style={{fontSize:"12px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                            </ul>
+                        </div>
+                    </section>
                 </div>
             </div>
         </Fragment>
