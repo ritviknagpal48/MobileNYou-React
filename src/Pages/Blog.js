94d16b8b5213bbeb8409c01d03eecc5fbcc88ae5
Shreyans Jain <jainshreyans121@gmail.com>
2021-01-04 19:07:01 +0530

    Blog Carasoul

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Navbar } from '../Components/Navbar';
+import { Card } from '../Components/Blogs/Card';
 
 export const Blog = () => {
 
@@ -9,6 +10,16 @@ export const Blog = () => {
             <div class="badydiv">
                 <div class="universalDiv" style={{backgroundColor:"#F5F5F5"}}>
                     <Navbar></Navbar>
+                    <section className="bodypanelmian aboutseller text-left">
+                        <div className="strike-color" style={{ textAlign: "center", marginTop: "10px", marginBottom: "24px" }}>
+                            <h6 className="strike-through-text"><span style={{margin:'0 0.2rem'}}>or</span></h6>
+                        </div>
+                        <div style={{margin:"12px 12px 12px 12px", whiteSpace:"nowrap", overflowX:"scroll", paddingBottom:"20px"}}>
+                            <Card></Card>
+                            <Card></Card>
+                            <Card></Card>
+                        </div>
+                    </section>
                 </div>
             </div>
         </Fragment>
