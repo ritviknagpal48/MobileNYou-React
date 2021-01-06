ab9a6b00c9e02bccf969024935b76de48f7a1405
Shreyans Jain <jainshreyans121@gmail.com>
2021-01-06 21:08:37 +0530

    inverted-sides

--- src/Pages/ReferAFriendDetails.js
+++ src/Pages/ReferAFriendDetails.js
@@ -9,31 +9,17 @@ export const ReferAFriendDetails = () => {
                     <Navbar />
                     <section class="bodypanelmian aboutseller text-left"> 
                       <img src='/image/Refer/ReferAFriend-Details.png' width="100%" />
-                        <div className="sellerlocation" style={{backgroundColor:"white", borderTopLeftRadius: '100% 30px', borderTopRightRadius: '100% 30px'}}>
-
-
-
-
-                        <p style={{fontSize:"11px"}}>Share Your Referal Link and Invite Your Friends and Get</p>
-                        <p style={{fontSize:"22px",fontWeight:"bold",color:"#c2172e",marginBottom:"0rem"}}> &#8377;500</p>
-                        <span style={{color:"white",backgroundColor:"#0080FF",fontSize:"18px",padding:"0.5rem 2.5rem"}}>B276HJ</span>
-                        <br></br>
-                        <button type="button" class="btn btn-outline-primary" style={{marginTop:"-0.5rem",borderRadius:"1.4rem",width:"6.5rem",fontSize:"12px",padding:"0.2rem",marginBottom:"0.5rem"}} onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}}>Tap to copy</button>
-                        <p style={{marginBottom:"0.5rem"}}>Share Your Personal Invite Code Via</p>
-                        <div class="row" style={{width:"50%",marginLeft:"-2.4rem"}}>
-                            <div class="col-3">
-                                <i class='bx bxl-whatsapp' style={{backgroundColor:"#4DC45A",color:"white",fontSize:"28px",borderRadius:"50%",padding:"0.1rem",marginRight:"-1.6rem"}}></i>
-                            </div>
-                            <div class="col-3">
-                                <i class='bx bxl-facebook-circle' style={{fontSize:"36px",color:"#38529A",position:"relative",top:"-0.15rem"}} ></i>
-                            </div>
-                            <div class="col-3">
-                                <i class='bx bxl-twitter' style={{backgroundColor:"#07A4F0",color:"white",fontSize:"18px",borderRadius:"50%",padding:"0.4rem",marginRight:"-1.2rem"}}></i>
-                            </div>
-                            <div class="col-3">
-                                <i class='bx bxs-share-alt' style={{backgroundColor:"#c2172e",color:"white",fontSize:"18px",borderRadius:"50%",padding:"0.4rem"}}></i>
-                            </div>
-                        </div>
+                      <div className="sellerlocation" style={{backgroundColor:"#f5f5f5", position: 'relative', borderBottomLeftRadius: '100% 50px', borderBottomRightRadius: '100% 50px', boxShadow: '0 -3px 4px 1px gainsboro inset', top:'-1rem', zIndex:'2'}}></div>
+                      <div className="sellerlocation" style={{backgroundColor:"white", position: 'relative', top: '-3.3rem', boxShadow: '0px 4px 4px 1px gainsboro'}}>
+                          <br />
+                          <br />
+                          <br />
+                          <br />
+                          <br />
+                          <br />
+                          <br />
+                          <br />
+                          <br />
                         </div>
                     </section>
                 </div>
